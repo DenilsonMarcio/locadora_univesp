52ca3555cd2dd095b9c26a5cb0ce0d22ac87b707
dmarcios <denilsonmarcio@hotmail.com>
2023-11-15 17:36:47 -0300

    restart projeto locadora

--- src/main/java/br/com/moppahtech/locadora/model/entities/ClientModel.java
+++ src/main/java/br/com/moppahtech/locadora/model/entities/ClientModel.java
@@ -9,10 +9,8 @@ import java.util.UUID;
 @Entity
 @Table(name= "tb_client")
 public class ClientModel extends PersonModel {
-
     public ClientModel() {
     }
-
     public ClientModel(UUID id, String name, String address, Integer age) {
         super(id, name, address, age);
     }

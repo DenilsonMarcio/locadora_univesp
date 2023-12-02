8e6e9b4208b0a7dd0420c233de49620a4d23a292
dmarcios <denilsonmarcio@hotmail.com>
2023-12-02 01:27:12 -0300

    Inclusão de Basic Auth, ExceptionAdvice e Criptografia de senha para usuário.

--- src/main/java/br/com/moppahtech/locadora/exceptions/BusinessException.java
+++ src/main/java/br/com/moppahtech/locadora/exceptions/BusinessException.java
@@ -1,6 +1,8 @@
 package br.com.moppahtech.locadora.exceptions;
 
 public class BusinessException extends RuntimeException {
-    public BusinessException(String messasge, String exception) {
+    public BusinessException(String messasge, String exception) { // default implementation ignored
+    }
+    public BusinessException(String messasge) { // default implementation ignored
     }
 }
